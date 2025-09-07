5893b460ff12555aae5025ef401cb9cfeec1aa34
Kaan <155573872+KaanGlc@users.noreply.github.com>
2025-09-08 02:11:35 +0300

    Update test-mongodb.js

--- api/test-mongodb.js
+++ api/test-mongodb.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
   // ⭐⭐⭐ CORS HEADERS BİTTİ ⭐⭐⭐
-  import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb';
   try {
     const connectionString = process.env.MONGODB_URI;
     
